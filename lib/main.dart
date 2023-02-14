c04970ae2e74313c9b7006a684f799a566fb3128
idvan <idvan007@gmail.com>
2023-02-14 10:35:12 +0200

    error repare

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
-String textgame = 'Memorize game';
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -18,6 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  static const textgame = 'Memorize game';
   const MyHomePage({super.key});
 
 //  @override
@@ -25,15 +23,17 @@ class MyHomePage extends StatelessWidget {
 // }
 
 //class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(textgame),
+        title: const Text(textgame),
       ),
-      body: Center(
+      body: const Center(
         child: Text(textgame),
       ),
     );
   }
+
 }
