83186dce6e5000cf78a390573efd070824b153f9
idvan <idvan007@gmail.com>
2023-02-14 16:05:44 +0200

    work in progress

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,8 +11,70 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(title),
       ),
-      body: const Center(
-        child: Text(title),
+      body: Center(
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MyIcon(),
+              MgBox(),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              MgBox(),
+              MyIcon(),
+            ],
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
+
+class MyIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        border: Border.all(width: 3, color: Colors.blue),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: const Icon(
+        Icons.lightbulb,
+        color: Colors.green,
+        size: 32.0,
+      ),
+    );
+  }
+}
+
+class MgBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        border: Border.all(width: 3, color: Colors.blue),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          'Memorize game',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 18, // высота шрифта 18
+            fontWeight: FontWeight.w800,
+          ),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
