0fb8c8ffd3d6c0a65c99d61ad35f4ce0ada2d1da
idvan007 <idvan007@gmail.com>
2023-02-20 21:22:58 +0200

    task 4

--- lib/home_game.dart
+++ lib/home_game.dart
@@ -7,18 +7,45 @@ class MyHomeGame extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          minimum: const EdgeInsets.all(4),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(width: 3, color: Colors.blue),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: const Center(
-              child: Text('\u{1F680} ',style: TextStyle(fontSize: 60),),
-            ),
+        minimum: const EdgeInsets.all(4),
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _MyCard(myIcon: '\u{1F680}'),
+              _MyCard(myIcon: '\u{1F681}'),
+              _MyCard(myIcon: '\u{1F68C}'),
+              _MyCard(myIcon: '\u{1F682}'),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _MyCard extends StatelessWidget {
+  String myIcon = '\u{1F680}';
+
+  _MyCard({required this.myIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(width: 3, color: Colors.blue),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Align(
+          child: Text(
+            myIcon,
+            style: const TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
     );
   }
 }
