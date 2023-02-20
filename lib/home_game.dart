46c4245e2dcc96bb23ccbbbd3f9b1654a3966bda
idvan <idvan007@gmail.com>
2023-02-20 11:34:25 +0200

    task 3_2

--- lib/home_game.dart
+++ lib/home_game.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class MyHomeGame extends StatelessWidget {
   const MyHomeGame({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return
-      Padding(
-        padding: const  EdgeInsets.fromLTRB(5,40.0,5,5),
-        child:Container(
-          decoration: BoxDecoration(
-            // color: Colors.yellow,
-            border: Border.all(width: 3, color: Colors.blue),
-            borderRadius: BorderRadius.circular(12),
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(5,40,5,5,),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 3, color: Colors.blue),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Center(
+              child: Text('\u{1F680} '),
+            ),
           ),
-          /*child: const Icon(
-          Icons.rocket_launch_outlined,
-          color: Colors.green,
-          size: 64.0,
-        ),*/
-        child:const  Center(
-          child: Text('\u{1F680} '),
         ),
-          //Text('\u{1F680} '),
       ),
     );
   }
