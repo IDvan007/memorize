ad814f04d2b7048ed63aba1c2c0a32adb10edd16
idvan <idvan007@gmail.com>
2023-02-27 13:50:05 +0200

    roll back

--- lib/my_card.dart
+++ lib/my_card.dart
@@ -1,44 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MyCard extends StatefulWidget {
+class MyCard extends StatelessWidget {
   final String myIcon;
+  static  String  mIcon ='\u{1F680}';
 
   const MyCard({
     super.key,
     required this.myIcon,
   });
 
- String get getIcon  {
-    return myIcon;
-  }
-
-  @override
-  State<MyCard> createState() => MyCardState();
-}
-
-class MyCardState extends State<MyCard> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: _handleTap,
       child: Padding(
         padding: const EdgeInsets.only(top: 16, bottom: 16),
         child: Container(
           decoration: BoxDecoration(
-            color: _active ? Colors.blue : Colors.white,
+            color:  Colors.white,
             border: Border.all(width: 3, color: Colors.blue),
             borderRadius: BorderRadius.circular(12),
           ),
           child: Align(
-            child: Text(_active ? '' : Scaffold.of(context).,
+            child: Text( myIcon,
                 style: const TextStyle(fontSize: 50)),
           ),
         ),
